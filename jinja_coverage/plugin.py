dd20ea99fce38c7a5e85f8cf1f8564c9c3ac206b
jccardonar <jccardona82@gmail.com>
2020-10-29 10:00:51 -0500

    Using pathlib, getting templates in subfolders

--- jinja_coverage/plugin.py
+++ jinja_coverage/plugin.py
@@ -4,28 +4,39 @@ import os.path
 import coverage.plugin
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
+from pathlib import Path
 
 
 class JinjaPlugin(coverage.plugin.CoveragePlugin):
     def __init__(self, options):
-        self.template_directory = options.get("template_directory")
+        self.template_directory = Path(options.get("template_directory")).absolute()
         self.environment = Environment(
             loader=FileSystemLoader(self.template_directory),
             extensions=[]
         )
 
+    def is_included(self, filename):
+        filename_path = Path(filename).absolute()
+        return self.template_directory in filename_path.parents
+
     def file_tracer(self, filename):
-        if os.path.samefile(os.path.dirname(filename), self.template_directory):
-            return FileTracer(filename)
+        #if os.path.samefile(os.path.dirname(filename), self.template_directory):
+        if self.is_included(filename):
+            return FileTracer(filename, self.template_directory)
 
     def file_reporter(self, filename):
-        if os.path.samefile(os.path.dirname(filename), self.template_directory):
+        #if os.path.samefile(os.path.dirname(filename), self.template_directory):
+        if self.is_included(filename):
             return FileReporter(filename, self.environment)
 
 
 class FileTracer(coverage.plugin.FileTracer):
-    def __init__(self, filename):
+    def __init__(self, filename, template_directory):
         self.metadata = {'filename': filename}
+        self.template_directory = template_directory
+        self.filename = Path(filename)
+        self.relative_path = self.filename.relative_to(self.template_directory)
+        self.relative_path_to_template = {}
 
     def source_filename(self):
         return self.metadata["filename"]
@@ -34,7 +45,17 @@ class FileTracer(coverage.plugin.FileTracer):
         lineno = -1
         env = frame.f_locals.get('environment')
         if env:
-            template = env.get_template(os.path.basename(frame.f_code.co_filename))
+            # get relative from templates. Not always matches.
+            #template_file = Path(frame.f_code.co_filename)
+            relative_path = self.filename.relative_to(self.template_directory)
+            if relative_path != self.relative_path:
+                raise Exception("Did not match expectation")
+            #template = env.get_template(os.path.basename(frame.f_code.co_filename))
+            template = self.relative_path_to_template.get(relative_path, None)
+            if template is None:
+                template = env.get_template(str(relative_path))
+                self.relative_path_to_template[relative_path] = template
+
             lineno = template.get_corresponding_lineno(frame.f_lineno)
 
         if lineno == 0:
